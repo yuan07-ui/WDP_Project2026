f5219c44865921855bd82bac63af503a65673463
yuan07-ui <yusef.andrei.prado@adamson.edu.ph>
2026-05-17 22:07:56 +0800

    Create script4.js

---
+++ script4.js
@@ -0,0 +1,9 @@
+function getInnerText() {
+alert(document.getElementById("example").innerText)
+}
+function getInnerHTML() {
+alert(document.getElementById("example").innerHTML)
+}
+function getTextContent() {
+alert(document.getElementById("example").textContent)
+}
